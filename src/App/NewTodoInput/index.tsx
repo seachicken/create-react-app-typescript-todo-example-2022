14dd5e05ec897d01d9d40d9a76524a30a8712987
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-06-20 00:26:51 +0900

    fix

--- src/App/NewTodoInput/index.tsx
+++ src/App/NewTodoInput/index.tsx
@@ -13,7 +13,6 @@ const NewTodoTextInput: React.FC = () => {
     createRef<HTMLInputElement>()
 
   function addTodo(e: React.KeyboardEvent<HTMLInputElement>): void {
-
     if (textInput.current === null) return
     if (e.shiftKey && e.key === 'Enter' && textInput.current.value.trim().length > 0) {
       // make new TODO object
